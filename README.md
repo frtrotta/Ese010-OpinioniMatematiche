651e7f8bad832539657be56ec50161c098904fae
Francesco Trotta <frtrotta@users.noreply.github.com>
2015-02-22 11:38:20 +0100

    Completato

---
+++ README.md
@@ -0,0 +1,12 @@
+# Ese010-OpinioniMatematiche
+
+Esegui il codice nel progetto e rispondi alle seguenti domande.
+
+Domande
+A. Cosa accade in `Stampa 001` e `Stampa 002`?
+B. Cosa accade in `Stampa 003` e `Stampa 004`?
+C. Cosa accade in `Stampa 101` e `Stampa 102`?
+D. Cosa accade in `Stampa 103` e `Stampa 104`?
+E. Qual è l'errore del risultato in `Stampa 201`?
+F. Qual è l'errore del risultato in `Stampa 202`?
+G. Cosa accade in `Stampa 201` e `Stampa 202`?
---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char** argv) {
+    int n_bit;
+    int a, b;
+    uint ua;
+    float f;
+    float fa, fb;
+    
+    printf("Il tipo di dato \"int\" memorizza interi _con_ segno e occupa %d byte\n", sizeof(int));
+    n_bit = sizeof(int) * 8; // dimensione in bit
+    a = 17;
+    printf("Stampa 000: %d + 1 = %d\n", a, a+1);
+    a = pow(2, n_bit-1) - 1;
+    printf("Stampa 001: %d + 1 = %d\n", a, a+1);
+    printf("Stampa 002: %d + 10 = %d\n", a, a+10);
+    a = -pow(2, n_bit-1);
+    printf("Stampa 003: %d - 1 = %d\n", a, a-1);
+    printf("Stampa 004: %d - 10 = %d\n", a, a-10);
+    
+    printf("\nIl tipo di dato \"uint\" memorizza interi _senza_ segno e occupa %d byte. \"u\" sta per \"unsigned\".\n", sizeof(ushort));
+    ua = 123;
+    printf("Stampa 100: %u + 1 = %u\n", ua, ua+1);
+    ua = pow(2, n_bit) - 1;
+    printf("Stampa 101: %u + 1 = %u\n", ua, ua+1);
+    printf("Stampa 102: %u + 10 = %u\n", ua, ua+10);
+    ua = 0;
+    printf("Stampa 103: %u - 1 = %u\n", ua, ua-1);
+    printf("Stampa 104: %u - 10 = %u\n", ua, ua-10);
+    
+    
+    a = 10;
+    b = 2;
+    f = a/b;
+    printf("\nStampa 200: %d / %d = %f\n", a, b, f);
+    
+    a = 10;
+    b = 3;
+    f = a/b;
+    printf("Stampa 201: %d / %d = %f\n", a, b, f);
+    
+    a = 10;
+    b = 6;
+    f = a/b;
+    printf("Stampa 202: %d / %d = %f\n", a, b, f);
+    
+    // Se compili con Dev-C++, togli il commento dalla linea seguente
+    //system("pause");
+            
+    return (EXIT_SUCCESS);
+}
+
